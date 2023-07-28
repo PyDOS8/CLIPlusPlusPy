2ebc373a01f9c94f135ebad56e14b34f5f5a80c0
clipyplusplus <124309800+clipyplusplus@users.noreply.github.com>
2023-07-28 14:08:04 -0400

    Update Login.py

--- Login.py
+++ Login.py
@@ -10,7 +10,7 @@ def user():
         password_bytes = password.encode("UTF-8")
         key = Fernet.generate_key() # create the key
         encrypted_password = Fernet.encrypt(password_bytes)
-        with open("password.key", "w") as wf:
+        with open("key.key", "w") as wf:
             wf.write(key) #Write the key to the file
             wf.close() # Close the file 
         with open("password.txt", "w") as wf:
