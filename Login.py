641e58a347dcb70e017061ec232f9c8ad70a7b88
clipyplusplus <124309800+clipyplusplus@users.noreply.github.com>
2023-07-28 15:33:10 -0400

    Update Login.py

--- Login.py
+++ Login.py
@@ -1,22 +1,16 @@
 import os 
 from cryptography.fernet import Fernet
 def user():
-    tries = 0
-    if os.path.exists("username.txt"):
+    if not os.path.exists("username.txt"):
         os.system("CLI++Py.exe")
     else:
         username = input("Create a username > ")
-        password = input("Create a password > ")
-        password_bytes = password.encode("UTF-8")
-        key = Fernet.generate_key() # create the key
-        encrypted_password = Fernet.encrypt(password_bytes)
-        with open("key.key", "w") as wf:
-            wf.write(key) #Write the key to the file
-            wf.close() # Close the file 
-        with open("password.txt", "w") as wf:
-            wf.write(encrypted_password)
-            wf.close()
+        key = Fernet.generate_key() # Generates a key
+        f = Fernet(key)
+        password = input("Enter text > ").encode()
+        encrypted_text = f.encrypt(password) # Encrypt the password
+        with open("password.txt", "wb") as wf:
+            wf.write(encrypted_text) # write the encrypted text to the password
         with open("username.txt", "w") as wf:
             wf.write(username)
-            wf.close()
 user()
